fa2087626785de1e67190d86311fba2421c299c6
samuelsanchez-debug <samuel.sanchez@olcproperties.net>
2026-06-24 18:57:37 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import Formulario from "./Form";
+import Formulario from "./form";
 
 const path = window.location.pathname;
 
