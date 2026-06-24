26c53324f5975dac2539c6d9e4eea61df8b085ff
samuelsanchez-debug <samuel.sanchez@olcproperties.net>
2026-06-24 18:38:30 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import Formulario from "./Form";
 
-createRoot(document.getElementById('root')!).render(
+const path = window.location.pathname;
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement!);
+
+root.render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {path === "/solicitar" ? <Formulario /> : <App />}
+  </StrictMode>
+);
