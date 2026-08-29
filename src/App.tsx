f1346e8239249e99ff3425872f52ab9f4bd6cad6
samuelsanchez-debug <samuel.sanchez@olcproperties.net>
2026-08-29 12:38:40 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2372,7 +2372,7 @@ const[data,setData]=useState({clientes:[],colaboradores:[],trabajos:[],incidenci
   useEffect(()=>{
     const cargar=async()=>{
 const[c,col,t,inc]=await Promise.all([supabase.from('clientes').select('*').order('id'),supabase.from('colaboradores').select('*').order('id'),supabase.from('trabajos').select('*').order('id'),supabase.from('incidencias').select('*').order('id',{ascending:false})]);
-      setData({clientes:c.data||[],colaboradores:col.data||[],trabajos:(t.data||[]).map(x=>({...x,clienteId:x.cliente_id,colaboradorId:x.colaborador_id,presupuestoColaborador:x.presupuesto_colaborador,precioCliente:x.precio_cliente})),incidencias:[]});
+      setData({clientes:c.data||[],colaboradores:col.data||[],trabajos:(t.data||[]).map(x=>({...x,clienteId:x.cliente_id,colaboradorId:x.colaborador_id,presupuestoColaborador:x.presupuesto_colaborador,precioCliente:x.precio_cliente})),incidencias:inc.data||[]});
       setCargando(false);
     };
     cargar();  
